9c37f6db14074f0b6c9f1eb03402a795cd415cc8
ykaika <yonika2254@gmail.com>
2025-03-04 07:22:30 +0700

    Acara 11 Auth Login dan Register Username Pada Laravel

---
+++ TemplateNiceAdmin/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        // $this->middleware('guest')->except('logout');
+        // $this->middleware('auth')->only('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string',
+            'password' => 'required|string|min:6',
+        ]);
+        
+
+        // LAKUKAN PENGECEKAN, JIKA INPUTAN DARI USERNAME FORMATNYA ADALAH EMAIL,
+        // MAKA KITA AKAN MELAKUKAN PROSES AUTHENTICATION MENGGUNAKAN EMAIL, SELAIN ITU,
+        // AKAN MENGGUNAKAN USERNAME
+        $loginType = filter_var($request->username, FILTER_VALIDATE_EMAIL) ? 'email' : 'username';
+
+        // TAMPUNG INFORMASI LOGINNYA, DIMANA KOLOM TYPE PERTAMA BERSIFAT DINAMIS
+        $login = [
+            $loginType => $request->username,
+            'password' => $request->password
+        ];
+
+        // LAKUKAN LOGIN
+        if (Auth::attempt($login)) {
+            return redirect()->route('home');
+        }
+
+        // JIKA SALAH, MAKA KEMBALI KE LOGIN DAN TAMPILKAN NOTIFIKASI
+        return redirect()->route('login')->with('error', 'Email/Password salah!');
+    }
+}
--- TemplateNiceAdmin/routes/web.php
+++ TemplateNiceAdmin/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('index');
@@ -9,3 +11,10 @@ Route::get('/', function () {
 Route::group(['namespace' => 'App\Http\Controllers\Backend'], function () {
     Route::resource('dashboard','DashboardController');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [LoginController::class, 'login']);
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
