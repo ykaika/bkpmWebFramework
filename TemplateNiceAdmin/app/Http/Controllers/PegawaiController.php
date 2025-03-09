98bbbbdbeec26eda3bbfeb38d89784fa072c475a
ykaika <yonika2254@gmail.com>
2025-03-10 03:20:23 +0700

    Acara 17 Session dan Request

---
+++ TemplateNiceAdmin/app/Http/Controllers/PegawaiController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PegawaiController extends Controller
+{
+    public function index($nama)
+    {
+        return $nama;
+    }
+
+    // public function index(Request $request)
+    // {
+    //     return $request->segment(2);
+    // }
+
+    public function formulir()
+    {
+        return view('formulir');
+    }
+    
+    public function proses(Request $request)
+    {
+        $nama = $request->input('nama');
+        $alamat = $request->input('alamat');
+
+        return "Nama : " . $nama . ", Alamat : " . $alamat;
+    }
+}
--- TemplateNiceAdmin/routes/web.php
+++ TemplateNiceAdmin/routes/web.php
@@ -3,15 +3,17 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\PegawaiController;
 
 Route::get('/', function () {
     return view('index');
 });
 
 Route::group(['namespace' => 'App\Http\Controllers\Backend'], function () {
-    Route::resource('dashboard','DashboardController');
-    Route::resource('pendidikan','PendidikanController');
-    Route::resource('pengalaman_kerja','PengalamanKerjaController');
+    Route::resource('dashboard', 'DashboardController');
+    Route::resource('pendidikan', 'PendidikanController');
+    Route::resource('pengalaman_kerja', 'PengalamanKerjaController');
 });
 
 Auth::routes();
@@ -21,3 +23,13 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+// Acara 17
+Route::get('/session/create', [SessionController::class, 'create']);
+Route::get('/session/show', [SessionController::class, 'show']);
+Route::get('/session/delete', [SessionController::class, 'delete']);
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
