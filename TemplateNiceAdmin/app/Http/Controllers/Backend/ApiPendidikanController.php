2d37885e86207c37b408aa60b62698c59dd1a9aa
ykaika <yonika2254@gmail.com>
2025-03-15 11:23:39 +0700

    Acara 22 API (Application Programming Interface) (2)

--- TemplateNiceAdmin/app/Http/Controllers/Backend/ApiPendidikanController.php
+++ TemplateNiceAdmin/app/Http/Controllers/Backend/ApiPendidikanController.php
@@ -14,4 +14,49 @@ class ApiPendidikanController extends Controller
         $pendidikan = Pendidikan::all();
         return Response::json($pendidikan, 201);
     }
+
+    public function getPen($id)
+    {
+        $pendidikan = Pendidikan::find($id);
+
+        if (!$pendidikan) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Data tidak ditemukan!'
+            ], 404);
+        }
+
+        return response()->json($pendidikan, 200);
+    }
+
+    public function createPen(Request $request)
+    {
+        $pendidikan = Pendidikan::create($request->all());
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil ditambahkan!',
+            'data' => $pendidikan
+        ], 201);
+    }
+
+    public function updatePen($id, Request $request)
+    {
+        Pendidikan::find($id)->update($request->all());
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil dirubah!'
+        ], 201);
+    }
+
+    public function deletePen($id)
+    {
+        Pendidikan::destroy($id);
+
+        return response()->json([
+            'status' => 'ok',
+            'message' => 'Pendidikan berhasil dihapus!'
+        ], 201);
+    }
 }
--- TemplateNiceAdmin/bootstrap/app.php
+++ TemplateNiceAdmin/bootstrap/app.php
@@ -16,7 +16,3 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-
-$app->middleware([
-    \App\Http\Middleware\CorsMiddleware::class,
-]);
